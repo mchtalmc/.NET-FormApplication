14f67b7a068589a374fe396cf268b7fbc69c0227
Erdoğan Mücahit Alımcı <mcht.almc@gmail.com>
2024-04-04 14:05:44 +0300

    FixedForEach

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -14,7 +14,7 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View();
+        return View(Repository.Products);
     }
 
     public IActionResult Privacy()
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,11 @@ namespace FormApplicationCourse.Models
 {
     public class Product
     {
+        [Display(Name = "Product Id")]
         public int ProductId { get; set; }
+        [Display(Name = "Product Name")]
         public string? ProductName { get; set; }
+        [Display(Name = "Price")]
         public decimal ProductPrice { get; set; }
         public string? Image { get; set; }
         public bool IsActive { get; set; }
