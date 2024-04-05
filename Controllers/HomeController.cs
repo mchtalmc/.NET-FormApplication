4b34de38a21180ccce321e73e038300fc55482b3
Erdoğan Mücahit Alımcı <mcht.almc@gmail.com>
2024-04-05 11:19:59 +0300

    ProductVıewModel

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,20 +13,32 @@ public class HomeController : Controller
 
     }
 
+    [HttpGet]
     public IActionResult Index(string searchString, string category)
     {
         var products = Repository.Products;
+
         if (!string.IsNullOrEmpty(searchString))
         {
             ViewBag.SearchString = searchString;
             products = products.Where(p => p.ProductName.ToLower().Contains(searchString)).ToList();
         }
+
         if (!string.IsNullOrEmpty(category) && category != "0")
         {
             products = products.Where(p => p.CategoryId == int.Parse(category)).ToList();
         }
-        ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "CategoryName");
-        return View(products);
+
+        // ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name", category);
+
+        var model = new ProductViewModel
+        {
+            Products = products,
+            Categories = Repository.Categories,
+            SelectedCategory = category
+        };
+
+        return View(model);
     }
 
     public IActionResult Privacy()
